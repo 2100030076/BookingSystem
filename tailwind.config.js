5de92402a8eaad19d1c399430ff3d77d50111893
saikeerthana <2100030076@kluniversity.in>
2024-07-12 21:33:53 +0530

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
-    './index.html',
-    './src/**/*.{vue,js,ts,jsx,tsx,vue}',
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,vue}",
   ],
   theme: {
     extend: {},
@@ -9,4 +10,4 @@ module.exports = {
   plugins: [
     require('daisyui'),
   ],
-};
+}
--- vitest.config.js
+++ vitest.config.js
@@ -1,6 +1,14 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
+import { fileURLToPath } from 'node:url'
+import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
+import viteConfig from './vite.config'
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      environment: 'jsdom',
+      exclude: [...configDefaults.exclude, 'e2e/**'],
+      root: fileURLToPath(new URL('./', import.meta.url))
+    }
+  })
+)
